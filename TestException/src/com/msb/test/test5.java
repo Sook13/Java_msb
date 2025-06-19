2c1e87912a09ec30705d6a3ef71fa881f3c51626
Sook13 <2283118171@qq.com>
2025-06-19 15:30:31 +0800

    update

---
+++ TestException/src/com/msb/test/test5.java
@@ -0,0 +1,27 @@
+package com.msb.test;
+
+import java.util.Scanner;
+
+/**
+ * @Author: Sy
+ * @Date: 2025/6/19 - 06 - 19 - 15:20
+ * @Description: com.msb.test
+ * @Version: 1.0
+ */
+public class test5 {
+	public static void main(String[] args) {
+		devide();
+	}
+	public static void devide() {
+		//键入两个数字: 求商
+		System.out.println("请录入第一个数: ");
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		System.out.println("请录入第二个数: ");
+		Scanner sc1 = new Scanner(System.in);
+		int b = sc1.nextInt();
+		if (b == 0){
+			throw new RuntimeException();
+		}
+		System.out.println("两数之商为: " + a / b);}
+}
