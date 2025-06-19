5f8a71c51d6f9955f3d16d14884ba5a9ed7c4aac
Sook13 <2283118171@qq.com>
2025-06-19 17:19:08 +0800

    update

---
+++ TestPackage/src/com/msb/test1/test1.java
@@ -0,0 +1,17 @@
+package com.msb.test1;
+
+/**
+ * @Author: Sy
+ * @Date: 2025/6/19 - 06 - 19 - 16:37
+ * @Description: com.msb.test1
+ * @Version: 1.0
+ */
+public class test1 {
+	public static void main(String[] args) {
+		//构造器
+		Integer i1 = new Integer(12);
+		System.out.println(i1.toString());
+		Integer i2 = new Integer("21");
+		System.out.println(i2.toString());
+	}
+}
