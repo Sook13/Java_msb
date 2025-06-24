8f11d3ebd4b4b242385c21bd758e58d73d67b271
Sook13 <2283118171@qq.com>
2025-06-24 21:28:12 +0800

    update

---
+++ Test03Package/src/str/str1.java
@@ -0,0 +1,13 @@
+package str;
+
+/**
+ * @Author: Sy
+ * @Date: 2025/6/24 - 06 - 24 - 21:22
+ * @Description: str
+ * @Version: 1.0
+ */
+public class str1 {
+	public static void main(String[] args) {
+		String str1 = "<UNK>";
+	}
+}
